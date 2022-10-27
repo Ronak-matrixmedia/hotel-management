d7c93c5c1983d0ba982746f51b895cea785cf62a
Ronak <ronak@matrixnmedia.com>
2022-10-27 13:21:17 +0530

    first-comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Hotels from './pages/hotel/Hotels';
+import List from './pages/list/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/hotels' element={<List />} />
+      <Route path='/hotels/:id' element={<Hotels />} />
+     </Routes>
     </div>
   );
 }
